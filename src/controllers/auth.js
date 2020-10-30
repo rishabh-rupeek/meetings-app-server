40e1a3ac2d4e0089fa0e8282c768107abe6b4c65
Rishabh <rishabh.malik@rupeek.com>
2020-10-30 16:00:09 +0530

    login now working properly

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -7,50 +7,53 @@ async function loginUser( req, res, next ){
 
     const credentials = req.body;
     //console.log(credentials);
-    User.findOne({ "email": credentials.email })
-        .exec(( error, result ) => {
-            
-            if( error || !result || typeof result !== 'object' || Object.keys( result ).length === 0 ) {
-                if( error ) {
-                    error.status = 403;
-                    return next( error );
-                } else {
-                    //console.log(result);
-                    const error = new Error( 'unknown db error' );
-                    error.status = 500;
-                    return next( error );
-                }
+    User
+    .findOne( { email: credentials.email } )
+    .exec(( error, result ) => {
+        if( error || !result || typeof result !== 'object' || Object.keys( result ).length === 0 ) {
+            if( error ) {
+                error.status = 403;
+                return next( error );
+            } else {
+                const error = new Error( 'unknown db error' );
+                error.status = 500;
+                return next( error );
+            }
+        }
+
+        // check match of password
+        result.checkPassword( credentials.password, ( err, isMatch ) => {
+            if( err ) {
+                err.status = 401;
+                return next( err );
             }
 
-            result.checkPassword( credentials.password, (err, isMatch) => {
+            if( !isMatch ) {
+                // again some error   
+                const error = new Error();
+                error.message = "Incorrect Password";
+                error.status = 401;
+                return next( error ); 
+            }
 
-                if(err){
-                    err.status = 401;
-                    return next(err);
+            const claims = { email: result.email, userId: result._id };
+    
+            jwt.sign(claims, 'secretKey', {expiresIn: '24h'}, function( error, token ) {
+                console.log( 'jwt token generated' );
+
+                if( error ) {
+                    return res.status(401).json({ message: error.message });
                 }
 
-                //console.log(result);
-                const claims = {
-                    email : result.email,
-                    userId : result._id,
-                    name : result.name
-                }
-
-                jwt.sign(claims, 'secretKey', function(error, token){
-                    
-                    if( error ) {
-                        return res.status(401).json({ message: error.message });
-                    }
-
-                    res.status(200).json({
-                        token: token,
-                        email: result.email,
-                        name: result.name,
-                        userId: result._id
-                    });
+                res.status(200).json({
+                    message: 'Signed in sucessfully',
+                    token: token,
+                    email: result.email,
+                    name: result.name
                 });
             });
-        });
+        })
+    });  
 
 }
 
--- src/models/user.js
+++ src/models/user.js
@@ -18,41 +18,40 @@ const userSchema = new mongoose.Schema({
 
 const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-userSchema.path('email').validate( email => emailRegex.test( email.toLowerCase() ), 'Invalid email id format');
+userSchema.path( 'email' ).validate( email => emailRegex.test( email.toLowerCase() ), 'Invalid email id format' );
 
 const SALT_FACTOR = 10;
 
-userSchema.pre('save', function (done) {
-
+userSchema.pre( 'save', function( done ) {
     const user = this;
 
-    // password not updated
-    if( !user.isModified( 'password' ) ){
+    // password has not been updated
+    if( !user.isModified( 'password' ) ) {
         return done();
     }
 
-    // hash new password and save it
-    bcrypt.genSalt( SALT_FACTOR, (err,salt) => {
-
-        if(err){
-            return done(err);
+    // password has been updated - hash and save it
+    bcrypt.genSalt( SALT_FACTOR, ( err, salt ) => {
+        if( err ) {
+            return done( err );
         }
 
-        bcrypt.hash( user.password, salt, (err, hashedPassword) => {
-            if(err){
-                return done(err);
+        bcrypt.hash( user.password, salt, ( err, hashedPassword ) => {
+            if( err ) {
+                return done( err );
             }
 
             user.password = hashedPassword;
             done();
-        } );
-    } );
+        });
+    });
 });
 
-userSchema.methods.checkPassword = function( password, done){
-    bcrypt.compare( password, this.password, (err, isMatch) => {
-        done(err,isMatch);
-    })
-}
+userSchema.methods.checkPassword = function( password, done ) {
+    bcrypt.compare( password, this.password, ( err, isMatch ) => {
+        if(err) return done(err);
+        done( err, isMatch );
+    });
+};
 
 mongoose.model( 'user', userSchema );
